76306e153534fbfcb12e27aa90c73d40cc182a74
Suni1023 <146279223+Suni1023@users.noreply.github.com>
2023-11-27 15:22:44 -0800

    final version

--- Final Data Wrangling.R
+++ Final Data Wrangling.R
@@ -13,8 +13,21 @@ tiktok_transcriptionSet <- tiktok_transcriptionSet %>%
 tiktok_transcriptionSet <- tiktok_transcriptionSet %>%
   filter(!is.na(video_like_count), author_ban_status == "active", video_comment_count != 0)
 
+# Adding new categorical variable
+tiktok_transcriptionSet$duration_category <- cut(tiktok_transcriptionSet$video_duration_sec, 
+                                        breaks = c(0, 15, 60, Inf), 
+                                        labels = c("Short", "Medium", "Long"), 
+                                        right = FALSE)
+
+# Adding new continuous/numerical variable
+tiktok_transcriptionSet$engagement_rate <- (as.numeric(tiktok_transcriptionSet$video_like_count) +
+                                              as.numeric(tiktok_transcriptionSet$video_comment_count)) /
+                                              as.numeric(tiktok_transcriptionSet$video_view_count)
+
 tiktok_bgmSet <- select(tiktok_bgmSet, user_name, video_length, n_likes, n_shares, n_comments, n_plays, hashtags, song)
-# tiktok_bgmSet <- filter(tiktok_bgmSet, video_length >= 10)
 
 tiktok_df <- left_join(tiktok_transcriptionSet, tiktok_bgmSet, by = c("video_like_count" = "n_likes"))
 tiktok_df <- filter(tiktok_df, !is.na(song))
+
+write.csv(tiktok_df, "tiktok.csv", row.names=TRUE)
+
