c430d2525ad57bf1d6c74b6318a68ce0dc680525
TonySUW <octony21@uw.edu>
2023-12-08 13:21:32 -0800

    change

--- app.R
+++ app.R
@@ -6,66 +6,21 @@ library(shiny)
 
 songs_df <- read.csv("tiktok.csv") 
 
-analysis_view <- fluidPage(
-  titlePanel(""),
-  sidebarLayout(
-    sidebarPanel( 
-      h2("Control Panel"),
-      selectInput(
-        inputId = "char_name",
-        label = "Select a character",
-        choices = char_df$Character
-      )
-    ),
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Plot", plotOutput(outputId = "radar")),
-        tabPanel("Table", tableOutput(outputId = "Table"))
-      )
-    )
+about_view <- fluidPage(h1("What makes Tiktok so addictive?"),
+                        p(strong(em("Revealing the dynamic relationship between information quality and system quality.")),
   )
 )
 
-about_view <- fluidPage(
-  h1("About Page"),
-  p("This is about Mario Cart")
-)
 
-ui <- navbarPage(
-  "Final Project INFO201",
-  tabPanel("About", about_view),
-  tabPanel("Analysis", analysis_view)
+ui <- navbarPage(inverse = TRUE, "Final Project INFO201",
+  tabPanel("Intro", includeCSS("styles.css"), about_view)
 )
 #server stuff goes here 
 server <- function(input, output){
   #output$testing <- renderText({
   #return(input$char_name)
   #})
-  
-  make_radar_tb <- function(name){
-    data_pt <- filter(char_df, Character == name)
-    data_pt <- select(data_pt, -c(Character, Class))
-    
-    max_pt <- summarise_all(char_df, max)
-    max_pt <- select(max_pt, -c(Character, Class))
-    
-    min_pt <- summarise_all(char_df, min)
-    min_pt <- select(min_pt, -c(Character, Class))
-    
-    do.call("rbind", list(max_pt, min_pt, data_pt))
-  }
-  output$table <- renderTable({
-    return(make_radar_tb(input$char_name))
-  })
-  output$radar <- renderPlot({
-    tb <- make_radar_tb(input$char_name)
-    radarchart(tb)
-  })
-  #output$barchart <- renderPlot({
-  # ans_df <- filter(char_df, Character == input$char_name)
-  #p <- ggplot(ans_df, aes(x = Class, y = Speed)) + geom_bar(stat = "identity")
-  #return(p)
-  #})
+
 }
 #Make the app 
 shinyApp(ui, server)
