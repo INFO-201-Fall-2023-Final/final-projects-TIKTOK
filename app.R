33bccca5baa5b1486d7c869e0299cc3e9754bc5a
TonySUW <octony21@uw.edu>
2023-12-08 16:06:02 -0800

    app change

--- app.R
+++ app.R
@@ -4,14 +4,38 @@ library(ggplot2)
 library(shiny)
 # Sunny can edited it!
 
-tiktok_df <- read.csv("tiktok.csv") 
+source("Final Data Wrangling.R")
 
 
-about_view <- fluidPage(h1("What makes Tiktok so addictive?"),
-                        p(strong(em("Revealing the dynamic relationship between information quality and system quality.")),
-  )
+about_view <-   fluidPage(
+  titlePanel("How TikTok's background sound affect production traffic"),
+# br(),
+
+tags$style(HTML("
+    h2 {
+            background-color: #c0d6b7;
+            # background-image: url('https://miro.medium.com/v2/resize:fit:1400/1*XnhCJ4DuRt_7oqwUiwjWPA.png');
+            color: Black;
+            }")),
+
+mainPanel(
+  h3("Introduction"),
+  p("Tiktok has become one of the most young people's social media, most youth even addict to it.
+      We will measure how information and system quality affect addictive behavior by thoroughly 
+      examining user engagement patterns and content trends. Our goal is to educate readers with a 
+      comprehensive grasp of the dynamics of addiction and encourage them to consider the role that
+      social media plays in their lives by fusing real-world examples with sound statistical analysis."),
+  br(),
+  p(paste("Overall out of the 151 tiktok videos we looked at, there are ", like_count, "videos that have over
+            10,000 likes. And in these 151 videos, ", popular_music, "has been used most.")),
+  br(),
+  
+),
+div(style = "float: right; margin-left: 50px;",
+    tags$img(src = "https://miro.medium.com/v2/resize:fit:1400/1*XnhCJ4DuRt_7oqwUiwjWPA.png", 
+             width = 500, height = 300, class = "right-image")),
+                        
 )
-
 algorithm_view <- fluidPage(h1("What kinds of video gets promoted by Tiktok?"),
                             sidebarLayout(
                               sidebarPanel( 
@@ -19,7 +43,6 @@ algorithm_view <- fluidPage(h1("What kinds of video gets promoted by Tiktok?"),
                                 selectInput(
                                   inputId = "tiktok_ele",
                                   label = "Different elements of a video",
-                                  choices = tiktok_df$song
                                 )
                               ),
                               mainPanel(
@@ -36,9 +59,9 @@ algorithm_view <- fluidPage(h1("What kinds of video gets promoted by Tiktok?"),
 ui <- navbarPage(inverse = TRUE, "Final Project INFO201",
   tabPanel("Intro", includeCSS("styles.css"), about_view),
   
-  tabPanel("panel",
+  tabPanel("panel",)
            
-))
+)
 #server stuff goes here 
 server <- function(input, output){
   #output$testing <- renderText({
